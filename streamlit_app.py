5882c76c01cfe0ef04a256dd094c421b4124f8f1
Darrylwahab2009 <darrylwahab@gmail.com>
2025-06-13 21:40:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 Selamat menempuh hidup baru")
 st.write(
     "Belajar invest crypto bareng timorex jinpachi sama kalimasada"
 )
+st.image("Timothy_Ronald_saat_Remaja_Tahun_2014_X_timothyronald22_28f86b6c9f.jpeg", width=200)
+st.image("images.jpg", width=500)
